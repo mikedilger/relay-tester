e7ef078cd9d18b1b94dfa94971798b7888b84187
Mike Dilger <mike@mikedilger.com>
2024-09-29 09:16:03 +1300

    Better output for generate_keypair (it looked like it was wrapping as it shows the hex key)

--- src/bin/generate_keypair.rs
+++ src/bin/generate_keypair.rs
@@ -3,9 +3,9 @@ use nostr_types::PrivateKey;
 fn main() {
     let mut private_key = PrivateKey::generate();
     let public_key = private_key.public_key();
-    println!("PUBLIC: {}", public_key.as_bech32_string());
+    println!("PUBLIC:  {}", public_key.as_bech32_string());
 
-    println!("        {}", public_key.as_hex_string());
+    println!("    hex: {}", public_key.as_hex_string());
     println!("PRIVATE: {}", private_key.as_bech32_string());
-    println!("        {}", private_key.as_hex_string());
+    println!("    hex: {}", private_key.as_hex_string());
 }
